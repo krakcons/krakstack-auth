ea4fd1a97d6d81d71a77b52c999aab458d90de9a
Billy Hawkes <billyhawkes02@gmail.com>
2026-05-01 12:27:44 -0400

    task: update to drizzle rc official effect, removed patches

--- src/services/database.ts
+++ src/services/database.ts
@@ -1,9 +1,33 @@
+import * as PgDrizzle from "drizzle-orm/effect-postgres";
+import { Context, Effect, Layer, Redacted } from "effect";
+import { PgClient } from "@effect/sql-pg";
+import { types } from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
 
-import { relations, schema } from "@/db/schema";
+// @ts-ignore - TODO: Setup your own schema and remove this comment
+import { relations } from "@/db/schema";
 
-export const db = drizzle({
-  connection: process.env.DATABASE_URL ?? "",
-  schema,
+const PgLive = PgClient.layer({
+  url: Redacted.make(process.env.DATABASE_URL!),
+  types: {
+    getTypeParser: (typeId, format) => {
+      if ([1184, 1114, 1082, 1186, 1231, 1115, 1185, 1187, 1182].includes(typeId)) {
+        return (val: any) => val;
+      }
+      return types.getTypeParser(typeId, format);
+    },
+  },
+});
+
+export class DB extends Context.Service<DB>()("DB", {
+  make: Effect.gen(function* () {
+    const db = yield* PgDrizzle.makeWithDefaults({ relations });
+    return db;
+  }),
+}) {
+  static readonly layer = Layer.effect(this, this.make).pipe(Layer.provide(PgLive));
+}
+
+export const db = drizzle(process.env.DATABASE_URL!, {
   relations,
 });
