35bc1018fe1ce0cc080ddc6afc74b8dac607c5aa
Billy Hawkes <billyhawkes02@gmail.com>
2026-06-11 13:47:49 -0400

    task: org switcher and messages

--- src/components/ui/organization-switcher.tsx
+++ src/components/ui/organization-switcher.tsx
@@ -225,7 +225,7 @@ const organizationDisplay = (organization: OrganizationSummary | null) => {
       organization?.name ||
       m.organization_switcher_label(),
     subtitle: organization?.slug ?? m.organization_switcher_label(),
-    logo: translation?.logo || "/logo192.png",
+    logo: translation?.logo || undefined,
   };
 };
 
