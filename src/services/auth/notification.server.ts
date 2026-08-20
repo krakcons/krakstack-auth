382df2a132273ac6e9f14ee86f79a0e267babcab
Billy Hawkes <billyhawkes02@gmail.com>
2026-08-20 15:55:12 -0400

    fix(email): provide notification persistence layer

--- src/services/auth/notification.server.ts
+++ src/services/auth/notification.server.ts
@@ -1,17 +1,27 @@
 import { Effect, Layer } from "effect";
-import { NotificationService } from "@krak-stack/registry/service-notification";
+import {
+  NotificationPersistenceStore,
+  NotificationService,
+} from "@krak-stack/registry/service-notification";
 import { NotificationChannelRegistry } from "@krak-stack/registry/service-notification/channels";
 import { SesNotificationChannel } from "@krak-stack/registry/notification-channel-email-ses";
 
 const sesNotificationRegistryLayer = Layer.effect(
   NotificationChannelRegistry,
   Effect.map(SesNotificationChannel, (channel) =>
-    NotificationChannelRegistry.make(channel),
+    NotificationChannelRegistry.make({
+      key: channel.key,
+      send: (_payload, message) =>
+        message.email === undefined
+          ? Effect.void
+          : channel.send(message.email, message),
+    }),
   ),
 ).pipe(Layer.provide(SesNotificationChannel.layer));
 
 const sesNotificationLayer = NotificationService.layer.pipe(
   Layer.provide(sesNotificationRegistryLayer),
+  Layer.provide(NotificationPersistenceStore.noopLayer),
 );
 
 export const notificationLayer =
